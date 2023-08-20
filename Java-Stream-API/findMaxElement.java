d790394ec81a70d439eea4fae5b3dd580a442c9c
Shubham Kaurav <56597563+shubhamkaurav97@users.noreply.github.com>
2023-08-20 23:08:13 +0530

    Create findMaxElement.java

---
+++ Java-Stream-API/findMaxElement.java
@@ -0,0 +1,15 @@
+// Given a list of integers, find the maximum value element present in it using Stream functions?
+
+import java.util.*;
+import java.util.stream.*;
+
+public class MyClass {
+    public static void main(String args[]) {
+      
+        List <Integer> eleList =  Arrays.asList(10,15,8,49,25,98,98,32,15);
+        
+        int maxElement = eleList.stream().max(Integer::compare).get();
+        
+        System.out.println(maxElement);
+    }
+}
