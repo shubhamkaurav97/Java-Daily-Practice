585caba7f230bbe0174a60e0b523955fd206dc59
Shubham Kaurav <56597563+shubhamkaurav97@users.noreply.github.com>
2023-08-20 23:16:48 +0530

    Create ListtoSet.java

---
+++ CollectionFramework/ListtoSet.java
@@ -0,0 +1,18 @@
+// Write a program to get distinct values present in a List using Set Interface.
+
+import java.util.*;
+import java.util.stream.*;
+
+public class MyClass {
+    public static void main(String args[]) {
+        List <Integer> eleList =  Arrays.asList(10,15,8,49,25,98,98,32,15);
+        
+        Set <Integer> eleSet = new HashSet<>();
+        
+        for(Integer s : eleList){
+            eleSet.add(s);
+        }
+        
+        System.out.println(eleSet);
+    }
+}
