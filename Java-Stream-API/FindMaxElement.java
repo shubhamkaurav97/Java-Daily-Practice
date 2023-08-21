6da5856af0fa7538c73c790f3fae8eae65f7b8e4
Shubham Kaurav <56597563+shubhamkaurav97@users.noreply.github.com>
2023-08-21 17:05:50 +0530

    Update and rename findMaxElement.java to FindMaxElement.java

--- Java-Stream-API/FindMaxElement.java
+++ Java-Stream-API/FindMaxElement.java
@@ -3,7 +3,7 @@
 import java.util.*;
 import java.util.stream.*;
 
-public class MyClass {
+public class FindMaxElement {
     public static void main(String args[]) {
       
         List <Integer> eleList =  Arrays.asList(10,15,8,49,25,98,98,32,15);
