4db8609aae0a19e000a27c6fe4e25a857bfc69e6
Shubham Kaurav <56597563+shubhamkaurav97@users.noreply.github.com>
2023-08-21 17:04:30 +0530

    Update UsingMaptoFindCountofElements.java

--- CollectionFramework/UsingMaptoFindCountofElements.java
+++ CollectionFramework/UsingMaptoFindCountofElements.java
@@ -3,7 +3,7 @@
 import java.util.*;
 import java.util.stream.*;
 
-public class MyClass {
+public class UsingMaptoFindCountofElements {
     public static void main(String args[]) {
         List <Integer> eleList =  Arrays.asList(2, 5, 8, 2, 10, 5, 15, 8, 20, 25, 30, 15, 10, 35, 40, 45, 20, 50, 55, 60);
         
