1505fad100f2181d3bd49ffac68a1e34eabbc927
Shubham Kaurav <56597563+shubhamkaurav97@users.noreply.github.com>
2023-08-22 23:59:41 +0530

    Create PrintPairofNumbersInAnArrayHavingSumEqualtoTargetNumber.java

---
+++ BasicQuestions/PrintPairofNumbersInAnArrayHavingSumEqualtoTargetNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        int arr[]={6,5,3,8,9,1,2,4,7};
+        int target =  8;
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println(arr[i]+" "+arr[j]);
+                }
+            }
+        }
+        
+
+     }
+}
