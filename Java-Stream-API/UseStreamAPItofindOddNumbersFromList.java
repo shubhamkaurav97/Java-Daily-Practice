e21829de6adc11ebde1e8a0e05f688906b7d3d5e
Shubham Kaurav <56597563+shubhamkaurav97@users.noreply.github.com>
2023-08-27 00:00:53 +0530

    Update UseStreamAPItofindOddNumbersFromList.java

--- Java-Stream-API/UseStreamAPItofindOddNumbersFromList.java
+++ Java-Stream-API/UseStreamAPItofindOddNumbersFromList.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class UseStreamAPItofindOddNumbersFromList {
     public static void main(String args[]) {
-      ArrayList <Integer> arrList = new ArrayList<>(Arrays.asLIst(1,2,4,99,44,33,11));
+      ArrayList <Integer> arrList = new ArrayList<>(Arrays.asList(1,2,4,99,44,33,11));
       
       arrList.stream().filter(x->x%2 !=0).forEach(x->System.out.println(x));
     }
